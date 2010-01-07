128909ff5b31cff4ca2d4ee73c65460a17963561
Davor Rotim <rotim.davor@gmail.com>
2010-01-07 16:52:23 +0100

    new weather

--- bin/weather.sh
+++ bin/weather.sh
@@ -14,4 +14,4 @@ if [ -z $1 ]; then
 	exit 0;
 fi
 
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
