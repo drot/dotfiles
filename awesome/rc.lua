266cf5ef641356dae445826697ecc5d9ce7a2283
Davor Rotim <rotim.davor@gmail.com>
2010-02-15 15:23:48 +0100

    fl

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -415,7 +415,7 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "Xarchiver" },
       properties = { floating = true } },
-    { rule = { class = "gimp" },
+    { rule = { class = "Gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
