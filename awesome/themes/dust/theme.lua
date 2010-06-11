2482b9715165e756601a42d01eaed3199c7dc292
drot <drot@mother.(none)>
2010-06-11 22:51:49 +0200

    rpall

---
+++ awesome/themes/dust/theme.lua
@@ -0,0 +1,64 @@
+-- {{{ Main
+theme = {}
+theme.confdir       = awful.util.getdir("config")
+theme.wallpaper_cmd = { "sh /home/drot/.fehbg" }
+-- }}}
+
+-- {{{ Font
+theme.font      = "nu 9"
+-- }}}
+
+-- {{{ Colors
+theme.bg_normal     = "#222222"
+theme.bg_focus      = "#908884"
+theme.bg_urgent     = "#cd7171"
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#111111"
+theme.fg_urgent     = "#ffffff"
+
+theme.border_width  = "1"
+theme.border_normal = "#222222"
+theme.border_focus  = "#908884"
+theme.border_marked = "#91231c"
+-- }}}
+
+-- {{{ Widgets
+theme.fg_widget           = "#908884"
+theme.fg_center_widget    = "#c6c6c6"
+theme.fg_end_widget       = "#908884"
+theme.border_widget       = theme.bg_normal
+-- }}}
+
+-- {{{ Taglist
+theme.taglist_squares_sel = theme.confdir .. "/icons/taglist/squarefz.png"
+theme.taglist_squares_unsel = theme.confdir .. "/icons/taglist/squareza.png"
+-- }}}
+
+-- {{{ Layout
+theme.layout_tile       = theme.confdir .. "/icons/layouts/tile.png"
+theme.layout_tileleft   = theme.confdir .. "/icons/layouts/tileleft.png"
+theme.layout_tilebottom = theme.confdir .. "/icons/layouts/tilebottom.png"
+theme.layout_tiletop    = theme.confdir .. "/icons/layouts/tiletop.png"
+theme.layout_max        = theme.confdir .. "/icons/layouts/max.png"
+theme.layout_magnifier  = theme.confdir .. "/icons/layouts/magnifier.png"
+theme.layout_floating   = theme.confdir .. "/icons/layouts/floating.png"
+-- }}}
+
+-- {{{ Widget icons
+theme.widget_cpu    = theme.confdir .. "/icons/cpu.png"
+theme.widget_mem    = theme.confdir .. "/icons/ram.png"
+theme.widget_fs     = theme.confdir .. "/icons/disk.png"
+theme.widget_net    = theme.confdir .. "/icons/down.png"
+theme.widget_netup  = theme.confdir .. "/icons/up.png"
+theme.widget_mail   = theme.confdir .. "/icons/mail.png"
+theme.widget_date   = theme.confdir .. "/icons/time.png"
+theme.widget_sep    = theme.confdir .. "/icons/separator.png"
+theme.widget_music  = theme.confdir .. "/icons/mpd.png"
+theme.widget_weather  = theme.confdir .. "/icons/temp.png"
+theme.widget_uptime  = theme.confdir .. "/icons/ac.png"
+theme.widget_vol  = theme.confdir .. "/icons/vol.png"
+theme.widget_pkg  = theme.confdir .. "/icons/pac.png"
+-- }}}
+
+return theme
---
+++ awesome/vicious/widgets/init.lua
@@ -0,0 +1,24 @@
+---------------------------------------------------
+-- Vicious widgets for the awesome window manager
+---------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2010, Adrian C. <anrxc@sysphere.org>
+---------------------------------------------------
+
+-- {{{ Configure widgets
+require("vicious.widgets.cpu")
+require("vicious.widgets.thermal")
+require("vicious.widgets.uptime")
+require("vicious.widgets.mem")
+require("vicious.widgets.fs")
+require("vicious.widgets.net")
+require("vicious.widgets.gmail")
+require("vicious.widgets.pkg")
+require("vicious.widgets.mpd")
+require("vicious.widgets.ossvol")
+require("vicious.widgets.weather")
+require("vicious.widgets.date")
+-- }}}
+
+-- Vicious: widgets for the awesome window manager
+module("vicious.widgets")
