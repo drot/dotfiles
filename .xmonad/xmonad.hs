ce7c13cf6af9fada2cb76ff70defaa8a1ef51542
drot <rotim.davor@gmail.com>
2010-06-26 21:13:47 +0200

    rm mpd

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -63,7 +63,7 @@ myConfig = defaultConfig
 
 -- Layout configuration
 --
-myLayoutHook = onWorkspaces ["4","5","6"] float $ tabs ||| tile ||| mtile ||| full ||| float
+myLayoutHook = onWorkspaces ["5","6"] float $ tabs ||| tile ||| mtile ||| full ||| float
 	where
 		tile = named "[]=" $ Tall 1 (3/100) (1/2)
 		mtile = named "[M]" $ Mirror tile
