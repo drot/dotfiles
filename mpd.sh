0f961384b63e878216a1e9d5af7123689c14e034
drot <rotim.davor@gmail.com>
2010-07-01 22:09:31 +0200

    xm

---
+++ mpd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# mpd script for showing mpd song + artist in xmobar
+# add this to xmobarrc
+#  , Run Com "sh" ["/path/to/mpd.sh"] "mpd" 10 
+# special thanks to fumbles for the tip
+
+MPD="$(mpc | grep -)"
+
+if [ -z "$MPD" ]; then
+	echo "-"
+else
+	echo $MPD
+fi
