00bfe37cdea998597a127ef7e71f41b773a2af26
drot <rotim.davor@gmail.com>
2010-07-30 18:08:19 +0200

    sy

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -35,28 +35,30 @@ myFont = "-*-anorexia-*-*-*-*-*-*-*-*-*-*-*-*"
 
 myUhook = withUrgencyHook NoUrgencyHook myConfig
 
-myConfig = defaultConfig { terminal = "urxvtc"
-			 , focusFollowsMouse = True
-			 , borderWidth = 1
-			 , modMask = mod4Mask
-			 , workspaces = ["1","2","3","4","5","6","7","8","9"]
-			 , normalBorderColor = "#7C7C7C"
-			 , focusedBorderColor = "#FFB6B0"
-			 , keys = myKeys
-			 , layoutHook = myLayoutHook
-			 , manageHook = myManageHook
-			 }
+myConfig = defaultConfig { 
+  terminal = "urxvtc"
+  , focusFollowsMouse = True
+  , borderWidth = 1
+  , modMask = mod4Mask
+  , workspaces = ["1","2","3","4","5","6","7","8","9"]
+  , normalBorderColor = "#7C7C7C"
+  , focusedBorderColor = "#FFB6B0"
+  , keys = myKeys
+  , layoutHook = myLayoutHook
+  , manageHook = myManageHook
+  }
 
 -- Status bar style
 --
-myPP = defaultPP { ppTitle = xmobarColor "#FFB6B0" "" . wrap "<fc=#B6DCFF><</fc> " " <fc=#B6DCFF>></fc>" . shorten 50
-		 , ppCurrent = xmobarColor "#CEFFAC" "" . wrap "<fc=#B6DCFF>[</fc>" "<fc=#B6DCFF>]</fc>"
-		 , ppUrgent = xmobarColor "#B6DCFF" "" . wrap "<fc=#CEFFAC>[</fc>" "<fc=#CEFFAC>]</fc>"
-		 , ppSep = " <fc=#FFB6B0>:</fc> "
-		 , ppWsSep = " <fc=#FFB6B0>:</fc> "
-		 , ppLayout = xmobarColor "#CEFFAC" ""
-                 , ppSort = fmap (.scratchpadFilterOutWorkspace) getSortByTag
-                 }
+myPP = defaultPP { 
+  ppTitle = xmobarColor "#FFB6B0" "" . wrap "<fc=#B6DCFF><</fc> " " <fc=#B6DCFF>></fc>" . shorten 50
+  , ppCurrent = xmobarColor "#CEFFAC" "" . wrap "<fc=#B6DCFF>[</fc>" "<fc=#B6DCFF>]</fc>"
+  , ppUrgent = xmobarColor "#B6DCFF" "" . wrap "<fc=#CEFFAC>[</fc>" "<fc=#CEFFAC>]</fc>"
+  , ppSep = " <fc=#FFB6B0>:</fc> "
+  , ppWsSep = " <fc=#FFB6B0>:</fc> "
+  , ppLayout = xmobarColor "#CEFFAC" ""
+  , ppSort = fmap (.scratchpadFilterOutWorkspace) getSortByTag
+  }
 
 -- Layout configuration
 --
@@ -69,42 +71,46 @@ myLayoutHook = onWorkspace "3" tile $ onWorkspace "4" float $
     full = named "[ ]" $ Full
     float = named "><>" $ simplestFloat
 
-myManageHook = composeAll [ className =? "MPlayer" --> doFloat
-                          , className =? "Gimp" --> doFloat 
-                          , className =? "Conkeror" --> doShift "2"
-                          , className =? "Emacs" --> doShift "3"
-                          ] <+> namedScratchpadManageHook myScratch
+myManageHook = composeAll [ 
+  className =? "MPlayer" --> doFloat
+  , className =? "Gimp" --> doFloat 
+  , className =? "Conkeror" --> doShift "2"
+  , className =? "Emacs" --> doShift "3"
+  ] <+> namedScratchpadManageHook myScratch
 
 -- Scratchpad
 --
-myScratch = [ NS "music" "urxvtc -e ncmpcpp" (title =? "ncmpcpp")
-              (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
-            ]
+myScratch = [ 
+  NS "music" "urxvtc -e ncmpcpp" (title =? "ncmpcpp")
+  (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+  ]
 
 -- Tab style
 --
-myTabConfig = defaultTheme { fontName = myFont
-			   , decoHeight = 12
-			   , activeColor = "#000000"
-			   , activeBorderColor = "#FFB6B0"
-			   , activeTextColor = "#CEFFAC"
-			   , inactiveColor = "#000000"
-			   , inactiveBorderColor = "#7C7C7C"
-			   , inactiveTextColor = "#EEEEEE"
-			   , urgentColor = "#000000"
-			   , urgentBorderColor = "#FFB6B0"
-			   , urgentTextColor = "#B6DCFF" 
-			   }
+myTabConfig = defaultTheme { 
+  fontName = myFont
+  , decoHeight = 12
+  , activeColor = "#000000"
+  , activeBorderColor = "#FFB6B0"
+  , activeTextColor = "#CEFFAC"
+  , inactiveColor = "#000000"
+  , inactiveBorderColor = "#7C7C7C"
+  , inactiveTextColor = "#EEEEEE"
+  , urgentColor = "#000000"
+  , urgentBorderColor = "#FFB6B0"
+  , urgentTextColor = "#B6DCFF" 
+  }
 
 -- Prompt style
 --
-myXPConfig = defaultXPConfig { font = myFont
-			     , fgColor = "#FFFFFF"
-			     , bgColor = "#000000"
-			     , bgHLight = "#000000"
-			     , fgHLight = "#CEFFAC"
-			     , position = Bottom 
-			     }
+myXPConfig = defaultXPConfig { 
+  font = myFont
+  , fgColor = "#FFFFFF"
+  , bgColor = "#000000"
+  , bgHLight = "#000000"
+  , fgHLight = "#CEFFAC"
+  , position = Bottom 
+  }
 
 -- Key bindings
 --
@@ -114,10 +120,10 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Main key bindings
 myKeys x  = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
-newKeys conf@(XConfig {XMonad.modMask = modm}) =
+newKeys conf@(XConfig {XMonad.modMask = modm}) = [
 
   -- launch terminal
-  [ ((modm, xK_Return), spawn $ XMonad.terminal conf)
+  ((modm, xK_Return), spawn $ XMonad.terminal conf)
   
     -- launch player
   , ((modm, xK_r), namedScratchpadAction myScratch "music")
