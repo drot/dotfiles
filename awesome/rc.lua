c3a05b9517df4a2a1e3913b323331a3eb2baa1e5
drot <rotim.davor@gmail.com>
2010-09-25 22:31:54 +0200

    sp del

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -341,17 +341,17 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey,           }, "l", function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey,           }, "h", function () awful.tag.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Shift"   }, "h", function () awful.tag.incnmaster( 1)      end),
+    awful.key({ modkey, "Shift"   }, "l", function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Control" }, "h", function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Control" }, "l", function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () promptbox[mouse.screen]:run() end),
+    awful.key({ modkey }, "r", function () promptbox[mouse.screen]:run() end),
 
     awful.key({ modkey }, "x",
               function ()
