09b7fb26bfab38c95b31c37eb8b22af7e63bdd53
Davor Rotim <rotim.davor@gmail.com>
2011-02-16 12:17:50 +0100

    change look

--- .gnus.el
+++ .gnus.el
@@ -21,22 +21,15 @@
       smtpmail-smtp-service 587)
 
 ;; Summary look
-(copy-face 'gnus-summary-high-unread 'gnus-face-6)
-(setq gnus-face-6 'gnus-face-6)
-(copy-face 'gnus-summary-normal-unread 'gnus-face7)
-(setq gnus-face-7 'gnus-face-7)
-(copy-face 'gnus-summary-normal-read 'gnus-face-8)
-(setq gnus-face-8 'gnus-face-8)
-(copy-face 'gnus-summary-normal-undownloaded 'gnus-face-9)
-(setq gnus-face-9 'gnus-face-9)
-(setq gnus-summary-make-false-root 'dummy)
-(setq gnus-summary-make-false-root-always nil)
-(setq gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
-      gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
-      gnus-sum-thread-tree-indent " "
-      gnus-sum-thread-tree-root "┏● " 
-      gnus-sum-thread-tree-false-root " ○ "
-      gnus-sum-thread-tree-single-indent " ● "
-      gnus-sum-thread-tree-leaf-with-other "┣━━❯ " 
-      gnus-sum-thread-tree-vertical "┃"
-      gnus-sum-thread-tree-single-leaf "┗━━❯ ")
+(setq-default
+ gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f %* %B%s%)\n"
+ gnus-user-date-format-alist '((t . "%d.%m.%Y %H:%M"))
+ gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+ gnus-thread-sort-functions '(gnus-thread-sort-by-date)
+ gnus-sum-thread-tree-false-root ""
+ gnus-sum-thread-tree-indent " "
+ gnus-sum-thread-tree-leaf-with-other "├► "
+ gnus-sum-thread-tree-root ""
+ gnus-sum-thread-tree-single-leaf "╰► "
+ gnus-sum-thread-tree-vertical "│"
+ )
