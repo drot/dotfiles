66ed78aad923fb4783c3834d14b94d5c6323524b
drot <rotim.davor@gmail.com>
2013-02-07 00:50:33 +0100

    small improvements

--- .emacs.d/.ercrc.el
+++ .emacs.d/.ercrc.el
@@ -1,19 +1,14 @@
 ;; spelling
 (erc-spelling-mode 1)
 
+;; disable buttonization
+(erc-button-mode nil)
+
 ;; colorize nicks
 (and
  (require 'erc-highlight-nicknames)
  (add-to-list 'erc-modules 'highlight-nicknames)
  (erc-update-modules))
-
-;; logging
-(setq erc-log-insert-log-on-open nil
-      erc-log-channels-directory "~/.emacs.d/logs/"
-      erc-log-file-coding-system 'utf-8
-      erc-save-buffer-on-part nil
-      erc-save-queries-on-quit nil)
-(add-hook 'erc-insert-post-hook 'erc-save-buffer-in-logs)
 
 ;; look
 (setq erc-nick-uniquifier "_"
@@ -28,7 +23,20 @@
       erc-timestamp-only-if-changed-flag nil
       erc-hide-list '("JOIN" "PART" "QUIT"))
 
-;; dynamic fill width
+;; logging
+(setq erc-log-insert-log-on-open nil
+      erc-log-channels-directory "~/.emacs.d/logs/"
+      erc-log-file-coding-system 'utf-8
+      erc-save-buffer-on-part nil
+      erc-save-queries-on-quit nil)
+(add-hook 'erc-insert-post-hook 'erc-save-buffer-in-logs)
+
+;; queries in new buffers, notices in current buffer
+(setq erc-auto-query t
+      erc-auto-query 'bury
+      erc-echo-notice-always-hook '(erc-echo-notice-in-active-buffer))
+
+;; dynamic fill width for buffers
 (make-variable-buffer-local 'erc-fill-column)
 (add-hook 'window-configuration-change-hook
 	  '(lambda ()
@@ -39,3 +47,9 @@
 		    (set-buffer buffer)
 		    (when (eq major-mode 'erc-mode)
 		      (setq erc-fill-column (- (window-width w) 2)))))))))
+
+;; truncate buffers
+(setq erc-max-buffer-size 20000)
+(defvar erc-insert-post-hook)
+(add-hook 'erc-insert-post-hook 'erc-truncate-buffer)
+(setq erc-truncate-buffer-on-save t)
