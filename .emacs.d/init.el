0ff4281fde624f7937ea4ae0a7f88851c311ce5d
drot <rotim.davor@gmail.com>
2013-02-26 23:13:04 +0100

    cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,31 +34,39 @@
 ;; Show unfinished keystrokes early
 (setq echo-keystrokes 0.1)
 
+;; Highlight matching parentheses
+(show-paren-mode 1)
+
 ;; Message buffer size
 (setq message-log-max 100)
 
 ;; Show column number in modeline
 (setq column-number-mode t)
 
-;; Highlight matching parentheses
-(show-paren-mode 1)
+;; Encoding
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 
-;; Colored output fix
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;; Edit files in compressed archives
+(auto-compression-mode t)
 
 ;; X clipboard copy and paste
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 
-;; Edit files in compressed archives
-(auto-compression-mode t)
-
-;; Enable Easy PG
-(require 'epa-file)
-
 ;; Ediff window placement
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 (setq ediff-split-window-function 'split-window-horizontally)
+
+;; Colored output fix
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; Enable Easy PG
+(require 'epa-file)
 
 ;; Make buffer names unique
 (require 'uniquify)
@@ -67,11 +75,54 @@
 (setq uniquify-after-kill-buffer-p t)
 (setq uniquify-ignore-buffers-re "^\\*")
 
+;; Default major mode
+(setq default-major-mode 'text-mode)
+
+;; Wrap lines at 70 in text-mode
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;; Default browser
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "conkeror")
+
+;; SLIME
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
+;; PKGBUILD mode
+(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
+(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist))
+
+;; Abbreviations
+(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
+(setq save-abbrevs t)
+(quietly-read-abbrev-file)
+(setq default-abbrev-mode t)
+
+;; Change backup behavior to save in a specified directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/saves/"))
+      backup-by-copying      t
+      version-control        t
+      delete-old-versions    t
+      kept-new-versions      6
+      kept-old-versions      2)
+
 ;; Save minibuffer history
 (setq savehist-additional-variables
       '(search-ring regexp-search-ring)
       savehist-file "~/.emacs.d/savehist")
 (savehist-mode t)
+
+;; Calendar
+(setq mark-holidays-in-calendar t
+      all-christian-calendar-holidays t
+      all-islamic-calendar-holidays nil
+      all-hebrew-calendar-holidays nil
+      european-calendar-style t
+      display-time-24hr-format t
+      calendar-latitude 43.20
+      calendar-longitude 17.48
+      calendar-location-name "Mostar, Bosnia and Herzegovina")
 
 ;; Use Ibuffer for buffer list
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -86,55 +137,3 @@
 ;; Icicles
 (require 'icicles)
 (icy-mode 1)
-
-;; Encoding
-(prefer-coding-system 'utf-8)
-(set-language-environment 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-
-;; Default browser
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "conkeror")
-
-;; Default major mode
-(setq default-major-mode 'text-mode)
-
-;; Wrap lines at 70 in text-mode
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
-;; SLIME
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-
-;; PKGBUILD mode
-(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
-(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist))
-
-;; Change backup behavior to save in a specified directory
-(setq backup-directory-alist '(("." . "~/.emacs.d/saves/"))
-      backup-by-copying      t
-      version-control        t
-      delete-old-versions    t
-      kept-new-versions      6
-      kept-old-versions      2)
-
-;; Calendar
-(setq
- mark-holidays-in-calendar t
- all-christian-calendar-holidays t
- all-islamic-calendar-holidays nil
- all-hebrew-calendar-holidays nil
- european-calendar-style t
- display-time-24hr-format t
- calendar-latitude 43.20
- calendar-longitude 17.48
- calendar-location-name "Mostar, Bosnia and Herzegovina")
-
-;; Abbreviations
-(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
-(setq save-abbrevs t)
-(quietly-read-abbrev-file)
-(setq default-abbrev-mode t)
