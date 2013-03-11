715c4b365ced6fe6c61c06985a37a8ea9841df34
drot <rotim.davor@gmail.com>
2013-03-11 18:58:13 +0100

    max lines

--- .emacs.d/elisp/rcirc-config.el
+++ .emacs.d/elisp/rcirc-config.el
@@ -22,7 +22,7 @@
 
 ; Max line width and number
 (setq rcirc-fill-column 'frame-width
-      rcirc-buffer-maximum-lines 2000)
+      rcirc-buffer-maximum-lines 500)
 
 ; Hide IRC spam
 (setq rcirc-omit-responses '("JOIN" "PART" "QUIT"))
