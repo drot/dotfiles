0ab6a4e57c883053bc27d665b47b494932015497
drot <rotim.davor@gmail.com>
2013-04-06 14:54:10 +0200

    change var names

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,19 +3,19 @@
 ;;
 
 ;; Define directories
-(defvar emacs-dir (file-name-directory load-file-name)
+(defvar drot-emacs-dir (file-name-directory load-file-name)
   "The root dir of the Emacs distribution.")
-(defvar core-dir (expand-file-name "core" emacs-dir)
+(defvar drot-core-dir (expand-file-name "core" drot-emacs-dir)
   "The home of Emacs core functionality.")
-(defvar modules-dir (expand-file-name "modules" emacs-dir)
+(defvar drot-modules-dir (expand-file-name "modules" drot-emacs-dir)
   "This directory houses all of the Emacs modules.")
-(defvar vendor-dir (expand-file-name "vendor" emacs-dir)
+(defvar drot-vendor-dir (expand-file-name "vendor" drot-emacs-dir)
   "This directory houses packages that are not yet available.")
 
 ;; Add to load path
-(add-to-list 'load-path core-dir)
-(add-to-list 'load-path modules-dir)
-(add-to-list 'load-path vendor-dir)
+(add-to-list 'load-path drot-core-dir)
+(add-to-list 'load-path drot-modules-dir)
+(add-to-list 'load-path drot-vendor-dir)
 
 ;; Core configuration
 (require 'drot-packages)
