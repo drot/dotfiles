e67cdf2e71fdc94db8714e1c0333c538ff623fbb
drot <rotim.davor@gmail.com>
2013-04-06 21:29:35 +0200

    switch back

--- .emacs.d/core/drot-core.el
+++ .emacs.d/core/drot-core.el
@@ -12,10 +12,9 @@
 ;; Message buffer size
 (setq message-log-max 100)
 
-;; Store all backup and auto-save files in the tmp dir
+;; Store all backup and auto-save files in the tmp directory
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
-(setq auto-save-list-file-prefix temporary-file-directory)
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
