fc5065527ebf663a917c079aea2614efd6be6498
drot <rotim.davor@gmail.com>
2013-04-09 14:12:12 +0200

    electric pair mode

--- .emacs.d/modules/drot-cc.el
+++ .emacs.d/modules/drot-cc.el
@@ -6,5 +6,12 @@
 (setq c-default-style "stroustrup"
       c-basic-offset 4)
 
+(defun drot-c-pair-mode ()
+  "Turn on Electric Pair mode."
+  (electric-pair-mode +1))
+
+;; Enable Electric Pair mode for all related languages
+(add-hook 'c-mode-common-hook 'drot-c-pair-mode)
+
 (provide 'drot-cc)
 ;; drot-cc.el ends here
