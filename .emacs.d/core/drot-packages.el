71d410611f1ee7a6bf09d667586e998ea4aa8c59
drot <rotim.davor@gmail.com>
2013-04-11 22:42:23 +0200

    enough

--- .emacs.d/core/drot-packages.el
+++ .emacs.d/core/drot-packages.el
@@ -4,7 +4,6 @@
 
 ;; Package sources
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
