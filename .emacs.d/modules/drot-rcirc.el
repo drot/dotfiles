9131b195491a03494c512aa32ea375a4100319d6
drot <rotim.davor@gmail.com>
2013-04-14 12:59:39 +0200

    keep it simple

--- .emacs.d/modules/drot-rcirc.el
+++ .emacs.d/modules/drot-rcirc.el
@@ -5,10 +5,6 @@
 ;; Load rcirc
 (require 'rcirc)
 
-; Colorize nicks and escape characters
-(require 'rcirc-color)
-(require 'rcirc-controls)
-
 (defun activate-bottom-prompt ()
   "Keep prompt at bottom."
   (set (make-local-variable 'scroll-conservatively) 8192))
@@ -30,13 +26,6 @@
     (load "~/.ircpass"))
 (setq rcirc-authinfo
       `(("freenode" nickserv "drot" ,freenode-password)))
-
-; Color palette for nicks
-(setq rcirc-colors '("#dca3a3" "#dfaf8f" "#f0dfaf"
-                     "#7f9f7f" "#93e0e3" "#cc9393"
-                     "#d0bf8f" "#afd8af" "#5c888b"
-                     "#dc8cc3" "#8cd0d3" "#94bff3"
-                     "#9fc59f" "#e0cf9f" "#6ca0a3"))
 
 ; Max line width and number
 (setq rcirc-fill-flag nil
