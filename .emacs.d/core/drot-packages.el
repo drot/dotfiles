1f43b603fcc0106f979fa822f55e1cb808090a52
drot <rotim.davor@gmail.com>
2013-04-14 15:27:42 +0200

    magit

--- .emacs.d/core/drot-packages.el
+++ .emacs.d/core/drot-packages.el
@@ -14,6 +14,7 @@
 (defvar drot-package-list '(auto-complete
                             diminish
                             paredit
+                            magit
                             pkgbuild-mode
                             undo-tree
                             yasnippet
