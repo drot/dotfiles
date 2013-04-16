14105ee0116d0a2ec5700ecbc0d2217c7cca7f80
drot <rotim.davor@gmail.com>
2013-04-16 19:33:52 +0200

    add for org

--- .emacs.d/core/drot-keybindings.el
+++ .emacs.d/core/drot-keybindings.el
@@ -11,5 +11,9 @@
 ;; Imenu with Ido completion
 (global-set-key (kbd "M-i") 'ido-goto-symbol)
 
+;; Org mode
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
 (provide 'drot-keybindings)
 ;; drot-keybindings.el ends here
