63f9f6b3feac16147fed12981e08a5be251b6f47
drot <rotim.davor@gmail.com>
2013-04-18 17:45:27 +0200

    for you

--- .emacs.d/core/drot-modules.el
+++ .emacs.d/core/drot-modules.el
@@ -2,32 +2,35 @@
 ;; drot-modules.el - Enable/disable modules
 ;;
 
-;; CC mode
-(require 'drot-cc)
-
-;; rcirc mode
-(require 'drot-rcirc)
-
 ;; Ido
 (require 'drot-ido)
 
 ;; Undo tree
 (require 'drot-undo)
 
+;; CC mode
+(require 'drot-cc)
+
+;; Org mode
+(require 'drot-org)
+
 ;; PKGBUILD mode
 (require 'drot-pkgbuild)
+
+;; rcirc mode
+(require 'drot-rcirc)
 
 ;; SLIME
 (require 'drot-slime)
 
 ;; ParEdit mode
-(eval-after-load 'drot-slime '(require 'drot-paredit))
+(require 'drot-paredit)
 
 ;; YASnippet
 (require 'drot-yasnippet)
 
 ;; Auto Complete
-(eval-after-load 'drot-yasnippet '(require 'drot-ac))
+(require 'drot-ac)
 
 (provide 'drot-modules)
 ;; drot-modules.el ends here
---
+++ .emacs.d/modules/drot-org.el
@@ -0,0 +1,12 @@
+;;
+;; drot-org.el - Configuration for Org mode
+;;
+
+;; Use Ido for completion
+(setq org-completion-use-ido t)
+
+;; Show timestamps
+(setq org-log-done 'time)
+
+(provide 'drot-org)
+;; drot-org.el ends here
