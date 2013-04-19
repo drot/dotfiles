93f079b020be2e9dcc8ad9481c2afcea0ad23454
drot <rotim.davor@gmail.com>
2013-04-19 12:38:09 +0200

    clang

--- .emacs.d/modules/drot-ac.el
+++ .emacs.d/modules/drot-ac.el
@@ -2,14 +2,33 @@
 ;; drot-ac.el - Configuration for Auto Complete
 ;;
 
-;; Enable Auto Complete
 (require 'auto-complete-config)
-(setq ac-comphist-file (expand-file-name "ac-comphist.dat" drot-saves-dir))
-(ac-config-default)
+(require 'auto-complete-clang-async)
 
-;; Fix conflict with YASnippet
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
+(defun my-ac-setup ()
+  "Change some defaults."
+  (setq ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (setq-default ac-comphist-file (expand-file-name "ac-comphist.dat" drot-saves-dir))
+  (setq ac-clang-complete-executable (expand-file-name "clang-complete" drot-vendor-dir))
+  (ac-set-trigger-key "TAB")
+  (ac-set-trigger-key "<tab>"))
+
+(defun my-ac-cc-setup ()
+  "Enable Clang for CC mode."
+  (setq ac-sources (append '(ac-source-clang-async ac-source-yasnippet) ac-sources))
+  (ac-clang-launch-completion-process))
+
+(defun my-ac-init ()
+  "Start Auto Complete for selected modes."
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (add-hook 'c-mode-common-hook 'my-ac-cc-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'my-ac-setup)
+  (global-auto-complete-mode t))
+
+;; Enable Auto Complete
+(my-ac-init)
 
 (provide 'drot-ac)
 ;; drot-ac.el ends here
