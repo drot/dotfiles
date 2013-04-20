58fbe6fc3d7721d12816495dadaea175d130e83e
drot <rotim.davor@gmail.com>
2013-04-20 23:02:39 +0200

    forgot to rm

--- .emacs.d/modules/drot-slime.el
+++
@@ -1,9 +0,0 @@
-;;
-;; drot-slime.el - Configuration for SLIME
-;;
-
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-
-(provide 'drot-slime)
-;; drot-slime.el ends here
