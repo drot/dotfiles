7a8399a4e2fdaa305be754e4ab8b57dddd21f869
drot <rotim.davor@gmail.com>
2013-04-22 13:41:30 +0200

    mv calendar

--- .emacs.d/core/drot-core.el
+++ .emacs.d/core/drot-core.el
@@ -64,18 +64,5 @@
 ;; Enable Winner mode
 (winner-mode 1)
 
-;; Calendar
-(setq calendar-mark-holidays-flag t
-      holiday-general-holidays nil
-      holiday-bahai-holidays nil
-      holiday-oriental-holidays nil
-      holiday-solar-holidays nil
-      holiday-islamic-holidays nil
-      holiday-hebrew-holidays nil
-      calendar-date-style 'european
-      calendar-latitude 43.20
-      calendar-longitude 17.48
-      calendar-location-name "Mostar, Bosnia and Herzegovina")
-
 (provide 'drot-core)
 ;; drot-core.el ends here
--- .emacs.d/modules/drot-org.el
+++ .emacs.d/modules/drot-org.el
@@ -2,6 +2,19 @@
 ;; drot-org.el - Configuration for Org mode
 ;;
 
+;; Calendar
+(setq calendar-mark-holidays-flag t
+      holiday-general-holidays nil
+      holiday-bahai-holidays nil
+      holiday-oriental-holidays nil
+      holiday-solar-holidays nil
+      holiday-islamic-holidays nil
+      holiday-hebrew-holidays nil
+      calendar-date-style 'european
+      calendar-latitude 43.20
+      calendar-longitude 17.48
+      calendar-location-name "Mostar, Bosnia and Herzegovina")
+
 ;; Use Ido for completion
 (setq org-completion-use-ido t)
 
