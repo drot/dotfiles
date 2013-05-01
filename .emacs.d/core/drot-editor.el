299df7129229611b49ac989acdf372a5eb7d36d7
drot <rotim.davor@gmail.com>
2013-05-01 16:58:30 +0200

    don't print messages

--- .emacs.d/core/drot-editor.el
+++ .emacs.d/core/drot-editor.el
@@ -73,6 +73,7 @@
 (require 'flyspell)
 (setq ispell-program-name "aspell"
       ispell-extra-args '("--sug-mode=ultra"))
+(setq flyspell-issue-message-flag nil)
 
 ;; Whitespace mode configuration
 (require 'whitespace)
