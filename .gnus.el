16374b5cf8c98884fa57371810303d66e43aaf42
drot <rotim.davor@gmail.com>
2013-05-16 23:36:38 +0200

    extra

--- .gnus.el
+++ .gnus.el
@@ -16,7 +16,6 @@
       message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
       smtpmail-auth-credentials "~/.authinfo.gpg"
-      smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587)
 
