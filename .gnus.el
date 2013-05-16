735f10c4e454d7ebe81a6a76de905d7e5f739973
drot <rotim.davor@gmail.com>
2013-05-16 20:57:38 +0200

    update

--- .gnus.el
+++ .gnus.el
@@ -20,15 +20,15 @@
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587)
 
-;; Summary look
-(setq-default
- gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f %* %B%s%)\n"
- gnus-user-date-format-alist '((t . "%d.%m.%Y %H:%M"))
- gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
- gnus-thread-sort-functions '(gnus-thread-sort-by-date)
- gnus-sum-thread-tree-false-root ""
- gnus-sum-thread-tree-indent " "
- gnus-sum-thread-tree-leaf-with-other "├► "
- gnus-sum-thread-tree-root ""
- gnus-sum-thread-tree-single-leaf "╰► "
- gnus-sum-thread-tree-vertical "│")
+;; Summary format
+(setq gnus-summary-line-format "%U%R%z %(%&user-date; %-15,15f %* %B%s%)\n"
+      gnus-user-date-format-alist '((t . "%d.%m.%Y %H:%M"))
+      gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+      gnus-thread-sort-functions '(gnus-thread-sort-by-date)
+      gnus-sum-thread-tree-false-root "┈┬──▷ "
+      gnus-sum-thread-tree-single-indent " ● "
+      gnus-sum-thread-tree-root "┌─▶ "
+      gnus-sum-thread-tree-vertical "│"
+      gnus-sum-thread-tree-leaf-with-other "├┬─► "
+      gnus-sum-thread-tree-single-leaf "╰┬─► "
+      gnus-sum-thread-tree-indent " ")
