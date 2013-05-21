68cd457afb2d75a928dd1401febe6e0b1161e39b
drot <rotim.davor@gmail.com>
2013-05-21 12:41:02 +0200

    simple

--- .emacs.d/modules/my-cc.el
+++ .emacs.d/modules/my-cc.el
@@ -4,10 +4,12 @@
 
 ;; General indentation style
 (setq c-basic-offset 4)
+(setq c-default-style '((java-mode . "java")
+                        (awk-mode . "awk")
+                        (other . "stroustrup")))
 
 (defun my-c-mode-hook ()
   "C mode setup"
-  (c-set-style "k&r")
   (unless (or (file-exists-p "makefile")
               (file-exists-p "Makefile"))
     (set (make-local-variable 'compile-command)
@@ -15,21 +17,12 @@
 
 (add-hook 'c-mode-hook 'my-c-mode-hook)
 
-;; C++ indentation style
-(c-add-style "my-c++-style"
-	     '("stroustrup"
-	       (c-offsets-alist . ((inline-open . 0)
-				   (brace-list-open . 0)
-				   (statement-case-open . +)))))
-
 (defun my-c++-mode-hook ()
   "C++ mode setup"
-  (c-set-style "my-c++-style")
-  (c-toggle-auto-hungry-state 1)
   (unless (or (file-exists-p "makefile")
-                        (file-exists-p "Makefile"))
-              (set (make-local-variable 'compile-command)
-                   (concat "g++ " (buffer-file-name) " -o "))))
+              (file-exists-p "Makefile"))
+    (set (make-local-variable 'compile-command)
+         (concat "g++ " (buffer-file-name) " -o "))))
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
