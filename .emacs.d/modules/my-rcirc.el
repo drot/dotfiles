be311bf5816b7510e6c2abff2c3661c126fefe86
drot <rotim.davor@gmail.com>
2013-06-28 18:07:12 +0200

    fix cond

--- .emacs.d/modules/my-rcirc.el
+++ .emacs.d/modules/my-rcirc.el
@@ -5,16 +5,16 @@
 ;; Load rcirc
 (require 'rcirc)
 
-; Connection
+;; Connection
 (setq rcirc-server-alist
       '(("adams.freenode.net" :channels ("#archlinux" "#emacs")
          :port 7000 :encryption tls)
         ("pine.forestnet.org" :channels ("#fo2")
          :port 6697 :encryption tls)))
 
-; Authorization
-(if (file-exists-p "~/.ircpass")
-    (load "~/.ircpass")
+;; Authorization
+(when (file-exists-p "~/.ircpass")
+  (load "~/.ircpass")
   (setq rcirc-authinfo
         `(("freenode" nickserv "drot" ,freenode-password)
           ("forestnet" nickserv "drot" ,freenode-password))))
@@ -25,21 +25,21 @@
                      "#E6DB74" "#66D9EF" "#F3ECB0"
                      "#CDF187" "#FC87B0" "#FEA7F7"))
 
-; Max line width and number
+                                        ; Max line width and number
 (setq rcirc-fill-flag t
       rcirc-fill-column 'frame-width
       rcirc-buffer-maximum-lines 1024)
 
-; Hide IRC spam
+                                        ; Hide IRC spam
 (setq rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY"))
 
-; Turn on Fly Spell mode
+                                        ; Turn on Fly Spell mode
 (add-hook 'rcirc-mode-hook 'flyspell-mode)
 
-; Turn on Omit mode
+                                        ; Turn on Omit mode
 (add-hook 'rcirc-mode-hook 'rcirc-omit-mode)
 
-; Track channel activity
+                                        ; Track channel activity
 (add-hook 'rcirc-mode-hook 'rcirc-track-minor-mode)
 
 (provide 'my-rcirc)
