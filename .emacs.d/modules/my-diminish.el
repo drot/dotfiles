eb029e7fa431d4a1236d835d455641ce8a167740
drot <rotim.davor@gmail.com>
2013-06-30 16:28:07 +0200

    dim

--- .emacs.d/modules/my-diminish.el
+++ .emacs.d/modules/my-diminish.el
@@ -3,13 +3,13 @@
 ;;
 
 ;; Shorten Abbrev mode
-(eval-after-load 'abbrev '(diminish 'abbrev-mode "Abv"))
+(eval-after-load "abbrev" '(diminish 'abbrev-mode "Abv"))
 
 ;; Shorten ParEdit
-(eval-after-load 'paredit '(diminish 'paredit-mode "PEd"))
+(eval-after-load "paredit" '(diminish 'paredit-mode "PEd"))
 
 ;; Shorten Undo Tree
-(eval-after-load 'undo-tree '(diminish 'undo-tree-mode "UTr"))
+(eval-after-load "undo-tree" '(diminish 'undo-tree-mode "UTr"))
 
 (provide 'my-diminish)
 ;; my-diminish.el ends here
