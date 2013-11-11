2ef01f0941d7f2182de1f65ba0e245d3e2ad5ed4
U-LAMBDA\drot <rotim.davor@gmail.com>
2013-11-11 21:03:57 +0100

    strict

--- .emacs.d/modules/my-smartparens.el
+++ .emacs.d/modules/my-smartparens.el
@@ -5,11 +5,18 @@
 ;; Load the default configuration
 (require 'smartparens-config)
 
-;; Enable smartparens strict mode
-(smartparens-global-strict-mode t)
+;; Enable smartparens mode
+(smartparens-global-mode t)
 
 ;; Highlight matching parens
 (show-smartparens-global-mode t)
+
+;; Enable smartparens strict mode for selected modes
+(add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
+(add-hook 'ielm-mode-hook 'smartparens-strict-mode)
+(add-hook 'lisp-mode-hook 'smartparens-strict-mode)
+(add-hook 'lisp-interaction-mode-hook 'smartparens-strict-mode)
+(add-hook 'scheme-mode-hook 'smartparens-strict-mode)
 
 (provide 'my-smartparens)
 ;; my-smartparens.el ends here
