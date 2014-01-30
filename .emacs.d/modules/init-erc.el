f9abd17a84dfc7361b978c0663c91d2f1d849479
U-LAMBDA\drot <rotim.davor@gmail.com>
2014-01-30 21:24:39 +0100

    not needed

--- .emacs.d/modules/init-erc.el
+++ .emacs.d/modules/init-erc.el
@@ -68,19 +68,10 @@
                        (concat "[" (symbol-name erc-network) "]")
                      (concat "[" (car erc-default-recipients) "]"))))
 
-(defun erc-button-url-previous ()
-  "Go to the previous URL button in this buffer."
-  (interactive)
-  (let* ((point (point))
-         (found (catch 'found
-                  (while (setq point (previous-single-property-change point 'erc-callback))
-                    (when (eq (get-text-property point 'erc-callback) 'browse-url)
-                      (throw 'found point))))))
-    (if found
-        (goto-char found)
-      (error "No previous URL button."))))
-
-(define-key erc-mode-map [backtab] 'erc-button-url-previous)
+(add-hook 'erc-mode-hook
+          (defun fix-scrolling-bug ()
+            "Keep the prompt at bottom"
+            (set (make-local-variable 'scroll-conservatively) 1000)))
 
 (provide 'init-erc)
 
