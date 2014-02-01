bb156fcf450007b76ae88c00fd883e16bef3ee83
U-LAMBDA\drot <rotim.davor@gmail.com>
2014-02-01 13:41:21 +0100

    keep for now

--- .emacs.d/modules/init-gnus.el
+++ .emacs.d/modules/init-gnus.el
@@ -1,5 +1,8 @@
 ;;; init-gnus.el --- Configuration for Gnus
 
+;; Load MIME decoding functions
+(require 'mm-decode)
+
 ;; User information
 (setq user-full-name "Davor Rotim")
 (setq user-mail-address "mlinfo@cock.li")
@@ -15,6 +18,20 @@
       smtpmail-smtp-server "mail.cock.li"
       smtpmail-smtp-service 587)
 
+;; Change Gnus default directories
+(setq gnus-directory "~/.gnus")
+(setq message-directory "~/.gnus/mail")
+(setq nnml-directory "~/.gnus/nnml-mail")
+(setq gnus-article-save-directory "~/.gnus/saved")
+(setq gnus-kill-files-directory "~/.gnus/scores")
+(setq gnus-cache-directory "~/.gnus/cache")
+
+;; Group topics
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+
+;; Group format
+(setq gnus-group-line-format "%M\%S\%p\%P\%5y:%B%(%g%)\n")
+
 ;; Message date format
 (setq gnus-user-date-format-alist
       '(((gnus-seconds-today) . "Today, %H:%M")
@@ -28,6 +45,33 @@
 (setq gnus-summary-line-format
       (concat "%U%R %~(max-right 17)~(pad-right 17)&user-date; "
 	      "%~(max-right 20)~(pad-right 20)n %B%s\n"))
+
+;; Summary tree format
+(setq gnus-sum-thread-tree-indent "  ")
+(setq gnus-sum-thread-tree-root "")
+(setq gnus-sum-thread-tree-false-root "o ")
+(setq gnus-sum-thread-tree-single-indent "")
+(setq gnus-sum-thread-tree-leaf-with-other "+-> ")
+(setq gnus-sum-thread-tree-vertical "| ") 
+(setq gnus-sum-thread-tree-single-leaf "`-> ")
+
+;; Kill the message buffer after sending a message
+(setq message-kill-buffer-on-exit t)
+
+;; Display images in messages
+(setq gnus-blocked-images nil)
+
+;; Display attached images
+(setq mm-inline-large-images 'resize
+      mm-inline-large-images-proportion 0.2)
+(add-to-list 'mm-attachment-override-types "image/.*")
+
+;; Display GnuPG signatures
+(setq gnus-buttonized-mime-types '("multipart/signed" "multipart/alternative"))
+(setq mm-verify-option 'always)
+
+;; Prefer plain text over HTML
+(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
 
 (provide 'init-gnus)
 
