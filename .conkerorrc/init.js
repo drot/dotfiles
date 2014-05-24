a02d188a8c7784c62c460f2fb89751cfcce11ba7
drot <rotim.davor@gmail.com>
2014-05-24 22:59:49 +0200

    add restore fun

--- .conkerorrc/init.js
+++ .conkerorrc/init.js
@@ -134,11 +134,44 @@ function history_clear () {
     var history = Cc["@mozilla.org/browser/nav-history-service;1"]
         .getService(Ci.nsIBrowserHistory);
     history.removeAllPages();
-}
+};
 
 interactive("history-clear",
             "Clear the history.",
             history_clear);
+
+// Restore killed buffer function
+var kill_buffer_original = kill_buffer_original || kill_buffer;
+
+var killed_buffer_urls = [];
+
+kill_buffer = function (buffer, force) {
+    if (buffer.display_uri_string) {
+        killed_buffer_urls.push(buffer.display_uri_string);
+    }
+
+    kill_buffer_original(buffer,force);
+};
+
+interactive("restore-killed-buffer-url", "Loads url from a previously killed buffer",
+            function restore_killed_buffer_url (I) {
+                if (killed_buffer_urls.length !== 0) {                
+                    var url = yield I.minibuffer.read(
+                        $prompt = "Restore killed url:",
+                        $completer = new all_word_completer($completions = killed_buffer_urls),
+                        $default_completion = killed_buffer_urls[killed_buffer_urls.length - 1],
+                        $auto_complete = "url",
+                        $auto_complete_initial = true,
+                        $auto_complete_delay = 0,
+                        $require_match = true);
+                    
+                    load_url_in_new_buffer(url);
+                } else {
+                    I.window.minibuffer.message("No killed buffer urls");
+                }
+            });
+
+define_key(content_buffer_normal_keymap, "C-x u", "restore-killed-buffer-url");
 
 // Eye guide keybindings
 define_key(content_buffer_normal_keymap, "space", "eye-guide-scroll-down");
