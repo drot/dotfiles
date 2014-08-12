c006299e3e1d1a61c5ac53f34945fd78ed275be7
drot <rotim.davor@gmail.com>
2014-08-12 16:25:38 +0200

    lexical

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,17 @@
-(defconst drot/emacs-directory (file-name-directory load-file-name)
+(defvar drot/emacs-directory (file-name-directory load-file-name)
   "Emacs root directory.")
 
-(defconst drot/configuration-file (expand-file-name "config.org" drot/emacs-directory)
+(defvar drot/configuration-file (expand-file-name "config.org" drot/emacs-directory)
   "Emacs configuration file written and loaded with Org-mode")
 
-(defconst drot/cache-directory (expand-file-name "cache" drot/emacs-directory)
+(defvar drot/custom-file (expand-file-name "custom.el" drot/emacs-directory)
+  "Store changes from the customize interface in the selected file.")
+
+(defvar drot/cache-directory (expand-file-name "cache" drot/emacs-directory)
   "This directory houses all cache files.")
 (make-directory drot/cache-directory t)
 
-(defconst drot/custom-file (expand-file-name "custom.el" drot/cache-directory)
-  "Store changes from the customize interface in the selected file.")
-
-(defconst drot/yas-directory (expand-file-name "snippets" drot/emacs-directory)
+(defvar drot/yas-directory (expand-file-name "snippets" drot/emacs-directory)
   "This directory houses all snippets.")
 (make-directory drot/yas-directory t)
 
