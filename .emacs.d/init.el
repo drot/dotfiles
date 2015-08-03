a6ba27e5663b1c26f9163267cc29fa7bda312edd
drot <rotim.davor@gmail.com>
2015-08-03 13:52:10 +0200

    make company behave like ac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -476,7 +476,24 @@
                            company-css
                            company-capf (company-dabbrev-code company-keywords)
                            company-files
-                           company-dabbrev)))
+                           company-dabbrev))
+
+  (define-key company-mode-map [remap indent-for-tab-command]
+    'company-indent-for-tab-command)
+
+  (setq tab-always-indent 'complete)
+
+  (defvar completion-at-point-functions-saved nil)
+
+  (defun company-indent-for-tab-command (&optional arg)
+    (interactive "P")
+    (let ((completion-at-point-functions-saved completion-at-point-functions)
+          (completion-at-point-functions '(company-complete-common-wrapper)))
+      (indent-for-tab-command arg)))
+
+  (defun company-complete-common-wrapper ()
+    (let ((completion-at-point-functions completion-at-point-functions-saved))
+      (company-complete-common))))
 
 ;; Easy-kill
 (use-package easy-kill
