a62747a4af56ebbc66062cd89c666bba495c707b
drot <rotim.davor@gmail.com>
2015-08-08 22:33:31 +0200

    chord polling too slow

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -790,19 +790,6 @@
   :ensure t
   :bind ("C-c t n" . nlinum-mode))
 
-;; Key chord
-(use-package key-chord
-  :ensure t
-  :config
-  (key-chord-define-global "jj" 'avy-goto-word-1)
-  (key-chord-define-global "jl" 'avy-goto-line)
-  (key-chord-define-global "jk" 'avy-goto-char)
-  (key-chord-define-global "js" 'swiper)
-  (key-chord-define-global "uu" 'undo-tree-visualize)
-  (key-chord-define-global "xx" 'execute-extended-command)
-  (key-chord-define-global "yy" 'browse-kill-ring)
-  (key-chord-mode 1))
-
 ;; Page break lines mode
 (use-package page-break-lines
   :ensure t
