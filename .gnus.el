98bf9f2a6ec0378aed989bab9ca4eb804afabbf7
drot <rotim.davor@gmail.com>
2015-12-17 22:21:26 +0100

    update gnus config

--- .gnus.el
+++ .gnus.el
@@ -28,34 +28,33 @@
 ;; Group topics
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
-;; Group format
-(setq gnus-group-line-format "%M\%S\%p\%P\%5y:%B%(%g%)\n")
+;; Configure visible headers
+(setq gnus-visible-headers
+      "^From:\\|^Reply-To\\|^Organization:\\|^To:\\|^Cc:\\|^Newsgroups:\\|^Subject:\\|^Date:\\|^Gnus")
 
-;; Summary format
-(setq gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n"
-      gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
+;; Show the article headers in this order
+(setq gnus-sorted-header-list
+      '("^From:" "^Reply-To" "^Organization:" "^To:" "^Cc:" "^Newsgroups:"
+        "^Subject:" "^Date:" "^Gnus"))
+
+;; Article fetching options
+(setq gnus-article-browse-delete-temp t
+      gnus-fetch-old-headers t
+      gnus-treat-strip-trailing-blank-lines 'last
+      gnus-mime-display-multipart-related-as-mixed t
+      gnus-auto-select-first nil)
+
+;; Display of the summary buffer
+(setq gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B (%c) %s%)\n"
+      gnus-user-date-format-alist '((t . "%d-%m-%Y %H:%M"))
+      gnus-group-line-format "%M%S%p%P%5y:%B %G\n" ;;"%B%(%g%)"
       gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
-      gnus-thread-sort-functions '(gnus-thread-sort-by-date))
-
-;; Summary tree format
-(setq gnus-sum-thread-tree-indent " "
+      gnus-thread-sort-functions '(gnus-thread-sort-by-most-recent-date)
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\”]\”[#’()]"
+      gnus-sum-thread-tree-false-root ""
+      gnus-sum-thread-tree-indent " "
+      gnus-sum-thread-tree-leaf-with-other "├► "
       gnus-sum-thread-tree-root ""
-      gnus-sum-thread-tree-false-root "o "
-      gnus-sum-thread-tree-single-indent ""
-      gnus-sum-thread-tree-leaf-with-other "+-> "
-      gnus-sum-thread-tree-vertical "| "
-      gnus-sum-thread-tree-single-leaf "`-> ")
-
-;; Display images in messages
-(setq gnus-blocked-images nil)
-
-;; Display attached images
-(setq mm-inline-large-images 'resize
-      mm-attachment-override-types '("image/.*"))
-
-;; Display GnuPG signatures
-(setq gnus-buttonized-mime-types '("multipart/signed" "multipart/alternative")
-      mm-verify-option 'always)
-
-;; Prefer plain text over HTML
-(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
+      gnus-sum-thread-tree-single-leaf "╰► "
+      gnus-sum-thread-tree-vertical "│"
+      smiley-style 'medium)
