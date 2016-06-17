2c985511328583fbf3182eed5814478d1b0f0f76
Davor Rotim <rotim.davor@gmail.com>
2016-06-17 12:56:07 +0200

    more visible

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -756,7 +756,7 @@
   :ensure t
   :bind ("C-c i y" . browse-kill-ring)
   :config
-  (setq browse-kill-ring-separator "-----------------------------------"
+  (setq browse-kill-ring-separator "———————————————————————————————————"
         browse-kill-ring-highlight-current-entry t
         browse-kill-ring-display-duplicates nil))
 
