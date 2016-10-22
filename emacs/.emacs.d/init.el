dba0d6ae13e6d43701b7e0d65bf9b5bc01e89ba8
Davor Rotim <rotim.davor@gmail.com>
2016-10-22 19:06:28 +0200

    don't add extra commands

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1040,7 +1040,7 @@ This doesn't support the chanserv auth method"
               ("C-c M-r" . slime-repl-previous-matching-input)
               ("C-c M-s" . slime-repl-next-matching-input))
   :config
-  ;; Disable conflicting key bindings with ParEdit
+  ;; Disable conflicting key bindings with Paredit
   (unbind-key "DEL" slime-repl-mode-map)
   (unbind-key "M-r" slime-repl-mode-map)
   (unbind-key "M-s" slime-repl-mode-map))
@@ -1281,68 +1281,7 @@ This doesn't support the chanserv auth method"
                   slime-repl-mode-hook
                   geiser-repl-mode-hook))
     (add-hook hook #'enable-paredit-mode))
-  :config
-  ;; Implement extreme barfage and slurpage commands
-  (defun paredit-barf-all-the-way-backward ()
-    (interactive)
-    (paredit-split-sexp)
-    (paredit-backward-down)
-    (paredit-splice-sexp))
-
-  (defun paredit-barf-all-the-way-forward ()
-    (interactive)
-    (paredit-split-sexp)
-    (paredit-forward-down)
-    (paredit-splice-sexp)
-    (if (eolp) (delete-horizontal-space)))
-
-  (defun paredit-slurp-all-the-way-backward ()
-    (interactive)
-    (catch 'done
-      (while (not (bobp))
-        (save-excursion
-          (paredit-backward-up)
-          (if (eq (char-before) ?\()
-              (throw 'done t)))
-        (paredit-backward-slurp-sexp))))
-
-  (defun paredit-slurp-all-the-way-forward ()
-    (interactive)
-    (catch 'done
-      (while (not (eobp))
-        (save-excursion
-          (paredit-forward-up)
-          (if (eq (char-after) ?\))
-              (throw 'done t)))
-        (paredit-forward-slurp-sexp))))
-
-  (nconc paredit-commands
-         '("Extreme Barfage & Slurpage"
-           (("C-M-)")
-            paredit-slurp-all-the-way-forward
-            ("(foo (bar |baz) quux zot)"
-             "(foo (bar |baz quux zot))")
-            ("(a b ((c| d)) e f)"
-             "(a b ((c| d)) e f)"))
-           (("C-M-}" "M-F")
-            paredit-barf-all-the-way-forward
-            ("(foo (bar |baz quux) zot)"
-             "(foo (bar|) baz quux zot)"))
-           (("C-M-(")
-            paredit-slurp-all-the-way-backward
-            ("(foo bar (baz| quux) zot)"
-             "((foo bar baz| quux) zot)")
-            ("(a b ((c| d)) e f)"
-             "(a b ((c| d)) e f)"))
-           (("C-M-{" "M-B")
-            paredit-barf-all-the-way-backward
-            ("(foo (bar baz |quux) zot)"
-             "(foo bar baz (|quux) zot)"))))
-
-  (paredit-define-keys)
-  (paredit-annotate-mode-with-examples)
-  (paredit-annotate-functions-with-examples)
-
+  :config  
   ;; Enable Paredit in suitable modes
   (defvar drot/paredit-minibuffer-commands '(eval-expression
                                              pp-eval-expression
