6464422ff18aa040695f80872e6f7a3b75d8a161
Davor Rotim <rotim.davor@gmail.com>
2016-12-10 18:16:57 +0100

    add urgency first

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -71,11 +71,6 @@
       *grab-pointer-foreground* (hex-to-xlib-color "#1e2320")
       *grab-pointer-background* (hex-to-xlib-color "#f0dfaf"))
 
-;; Window style
-(setf *window-border-style* :thin
-      *maxsize-border-width* 1
-      *mouse-focus-policy* :click)
-
 ;; Message and input prompt style
 (setf *input-window-gravity* :center
       *message-window-gravity* :bottom-right
@@ -92,7 +87,7 @@
 
 ;; Mode line format
 (setf *time-modeline-string* "^6*%d-%m^n ^3*%H:%M^n"
-      *screen-mode-line-format* '("^7*<%n>^n ^30%W^n ^06%u^n ^>"
+      *screen-mode-line-format* '("^7*<%n>^n ^06%u^n ^30%W^n ^>"
                                   "^2*%c^n> ^4*%M^n> ^7*%l^n> %d"))
 
 ;; Show the mode line for current screen
@@ -135,6 +130,12 @@
 
 (define-frame-preference "misc"
     (0 t t :class "Gimp"))
+
+(define-frame-preference "misc"
+    (0 t t :class "libreoffice"))
+
+(define-frame-preference "misc"
+    (0 t t :class "Zathura"))
 
 ;; Change default prefix key
 (set-prefix-key (kbd "C-i"))
