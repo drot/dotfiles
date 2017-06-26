f84f54baa956f60b218c7886f593a6f8d5c7ee34
Davor Rotim <rotim.davor@gmail.com>
2017-06-26 20:42:48 +0200

    make part of def

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -209,7 +209,6 @@
   (setq undo-tree-auto-save-history t)
   (setq undo-tree-visualizer-timestamps t)
   (setq undo-tree-visualizer-relative-timestamps t)
-
   ;; Compress Undo Tree history files by default
   (advice-add 'undo-tree-make-history-save-file-name :filter-return
               (lambda (return-value) (concat return-value ".gz"))))
