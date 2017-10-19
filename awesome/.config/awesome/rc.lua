bc680b2ccab4fec7abd8963808abc83ec711c7a7
Davor Rotim <rotim.davor@gmail.com>
2017-10-19 21:43:42 +0200

    change titlebar icons

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -829,11 +829,11 @@ client.connect_signal("request::titlebars", function(c)
                                layout  = wibox.layout.flex.horizontal
                             },
                             { -- Right
-                               awful.titlebar.widget.minimizebutton (c),
                                awful.titlebar.widget.floatingbutton (c),
-                               awful.titlebar.widget.maximizedbutton(c),
                                awful.titlebar.widget.stickybutton   (c),
                                awful.titlebar.widget.ontopbutton    (c),
+                               awful.titlebar.widget.minimizebutton (c),
+                               awful.titlebar.widget.maximizedbutton(c),
                                awful.titlebar.widget.closebutton    (c),
                                layout = wibox.layout.fixed.horizontal()
                             },
