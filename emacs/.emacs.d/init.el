d1e3d214426b08f0fd06cc6a2a27d2d092223c60
Davor Rotim <rotim.davor@gmail.com>
2017-11-18 13:58:50 +0100

    avoid clashes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -686,8 +686,8 @@
 ;; Configuration
 (after 'gnus
   ;; Set key bindings
-  (bind-key "M-o" #'ace-link-gnus gnus-summary-mode-map)
-  (bind-key "M-o" #'ace-link-gnus gnus-article-mode-map)
+  (bind-key "M-O" #'ace-link-gnus gnus-summary-mode-map)
+  (bind-key "M-O" #'ace-link-gnus gnus-article-mode-map)
   ;; Configure mail and news server
   (setq gnus-select-method '(nnimap "mail.cock.li"
                                     (nnimap-address "mail.cock.li")
@@ -787,7 +787,7 @@
 ;; Configuration
 (after 'org
   ;; Set key binding
-  (bind-key "M-o" #'ace-link-org org-mode-map)
+  (bind-key "M-O" #'ace-link-org org-mode-map)
   ;; Customize
   (setq org-directory (locate-user-emacs-file "org/"))
   (setq org-default-notes-file (locate-user-emacs-file "org/notes.org"))
