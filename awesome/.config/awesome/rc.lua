bf446ea9b44ce1178b4372bb70266c4042674c01
Davor.Rotim <rotim.davor@gmail.com>
2018-05-13 21:21:54 +0200

    mostly redundant

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -1030,7 +1030,7 @@ awful.rules.rules = {
                     keys = clientkeys,
                     buttons = clientbuttons,
                     screen = awful.screen.preferred,
-                    placement = awful.placement.centered+awful.placement.no_overlap+awful.placement.no_offscreen
+                    placement = awful.placement.no_overlap+awful.placement.no_offscreen
      }
    },
 
