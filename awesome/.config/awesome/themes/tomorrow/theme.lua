ea49f75c26a16f254189fdeddde1523ec4f0bd08
Davor.Rotim <rotim.davor@gmail.com>
2018-08-13 12:53:42 +0200

    [awesome] add hover titlebar buttons

--- awesome/.config/awesome/themes/tomorrow/theme.lua
+++ awesome/.config/awesome/themes/tomorrow/theme.lua
@@ -124,29 +124,39 @@ theme.menu_quit = themes_path .. "themes/tomorrow/icons/menu/quit.png"
 -- Titlebar
 theme.titlebar_close_button_normal = themes_path .. "themes/tomorrow/icons/titlebar/close_normal.png"
 theme.titlebar_close_button_focus = themes_path .. "themes/tomorrow/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_focus_hover = themes_path .. "themes/tomorrow/icons/titlebar/close_focus_hover.png"
 
 theme.titlebar_minimize_button_normal = themes_path .. "themes/tomorrow/icons/titlebar/minimize_normal.png"
 theme.titlebar_minimize_button_focus = themes_path .. "themes/tomorrow/icons/titlebar/minimize_focus.png"
+theme.titlebar_minimize_button_focus_hover = themes_path .. "themes/tomorrow/icons/titlebar/minimize_focus_hover.png"
 
 theme.titlebar_ontop_button_normal_inactive = themes_path .. "themes/tomorrow/icons/titlebar/ontop_normal_inactive.png"
 theme.titlebar_ontop_button_focus_inactive = themes_path .. "themes/tomorrow/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_focus_inactive_hover = themes_path .. "themes/tomorrow/icons/titlebar/ontop_focus_inactive_hover.png"
 theme.titlebar_ontop_button_normal_active = themes_path .. "themes/tomorrow/icons/titlebar/ontop_normal_active.png"
 theme.titlebar_ontop_button_focus_active = themes_path .. "themes/tomorrow/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_focus_active_hover = themes_path .. "themes/tomorrow/icons/titlebar/ontop_focus_active_hover.png"
 
 theme.titlebar_sticky_button_normal_inactive = themes_path .. "themes/tomorrow/icons/titlebar/sticky_normal_inactive.png"
 theme.titlebar_sticky_button_focus_inactive = themes_path .. "themes/tomorrow/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_focus_inactive_hover = themes_path .. "themes/tomorrow/icons/titlebar/sticky_focus_inactive_hover.png"
 theme.titlebar_sticky_button_normal_active = themes_path .. "themes/tomorrow/icons/titlebar/sticky_normal_active.png"
 theme.titlebar_sticky_button_focus_active = themes_path .. "themes/tomorrow/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_focus_active_hover = themes_path .. "themes/tomorrow/icons/titlebar/sticky_focus_active_hover.png"
 
 theme.titlebar_floating_button_normal_inactive = themes_path .. "themes/tomorrow/icons/titlebar/floating_normal_inactive.png"
 theme.titlebar_floating_button_focus_inactive = themes_path .. "themes/tomorrow/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_focus_inactive_hover = themes_path .. "themes/tomorrow/icons/titlebar/floating_focus_inactive_hover.png"
 theme.titlebar_floating_button_normal_active = themes_path .. "themes/tomorrow/icons/titlebar/floating_normal_active.png"
 theme.titlebar_floating_button_focus_active = themes_path .. "themes/tomorrow/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_focus_active_hover = themes_path .. "themes/tomorrow/icons/titlebar/floating_focus_active_hover.png"
 
 theme.titlebar_maximized_button_normal_inactive = themes_path .. "themes/tomorrow/icons/titlebar/maximized_normal_inactive.png"
 theme.titlebar_maximized_button_focus_inactive = themes_path .. "themes/tomorrow/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_focus_inactive_hover = themes_path .. "themes/tomorrow/icons/titlebar/maximized_focus_inactive_hover.png"
 theme.titlebar_maximized_button_normal_active = themes_path .. "themes/tomorrow/icons/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = themes_path .. "themes/tomorrow/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_focus_active_hover = themes_path .. "themes/tomorrow/icons/titlebar/maximized_focus_active_hover.png"
 
 -- Layouts
 theme.layout_cornerne = themes_path .. "themes/tomorrow/icons/layouts/cornerne.png"
