d5065f8b0bd860d844d3cf15bdd20c61a2a62a0f
Davor Rotim <rotim.davor@gmail.com>
2019-01-03 18:59:43 +0100

    [emacs] modify outline hydra

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -990,25 +990,38 @@
 ;; Configuration
 (after-load 'outline
   ;; Define Hydra
-  (defhydra hydra-outline (:columns 4)
-    "Outline Mode"
-    ("z" outline-hide-sublevels "Hide Sub-Levels")
-    ("t" outline-hide-body "Hide Body")
-    ("o" outline-hide-other "Hide Other")
-    ("c" outline-hide-entry "Hide Entry")
-    ("l" outline-hide-leaves "Hide Leaves")
-    ("d" outline-hide-subtree "Hide Sub-Tree")
-    ("a" outline-show-all "Show All")
-    ("e" outline-show-entry "Show Entry")
-    ("i" outline-show-children "Show Children")
-    ("k" outline-show-branches "Show Branches")
-    ("s" outline-show-subtree "Show Sub-Tree")
-    ("u" outline-up-heading "Up Heading")
-    ("n" outline-next-visible-heading "Next Visible Heading")
-    ("p" outline-previous-visible-heading "Previous Visible Heading")
-    ("f" outline-forward-same-level "Forward Same Level")
-    ("b" outline-backward-same-level "Backward Same Level")
-    ("q" nil "Quit"))
+  (defhydra hydra-outline (:color pink :hint nil)
+    "
+^Hide^             ^Show^           ^Move
+^^^^^^------------------------------------------------------
+_q_: sublevels     _a_: all         _u_: up
+_t_: body          _e_: entry       _n_: next visible
+_o_: other         _i_: children    _p_: previous visible
+_c_: entry         _k_: branches    _f_: forward same level
+_l_: leaves        _s_: subtree     _b_: backward same level
+_d_: subtree
+
+"
+    ;; Hide
+    ("q" hide-sublevels)    ; Hide everything but the top-level headings
+    ("t" hide-body)         ; Hide everything but headings (all body lines)
+    ("o" hide-other)        ; Hide other branches
+    ("c" hide-entry)        ; Hide this entry's body
+    ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
+    ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
+    ;; Show
+    ("a" show-all)          ; Show (expand) everything
+    ("e" show-entry)        ; Show this heading's body
+    ("i" show-children)     ; Show this heading's immediate child sub-headings
+    ("k" show-branches)     ; Show all sub-headings under this heading
+    ("s" show-subtree)      ; Show (expand) everything in this heading & below
+    ;; Move
+    ("u" outline-up-heading)                ; Up
+    ("n" outline-next-visible-heading)      ; Next
+    ("p" outline-previous-visible-heading)  ; Previous
+    ("f" outline-forward-same-level)        ; Forward - same level
+    ("b" outline-backward-same-level)       ; Backward - same level
+    ("z" nil "leave"))
   ;; Set local key binding
   (bind-key "C-c o h" #'hydra-outline/body outline-minor-mode-map))
 
