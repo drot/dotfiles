0e35fcf7e79d246293de15804f7bf36c313409cb
Davor Rotim <rotim.davor@gmail.com>
2019-08-09 15:28:20 +0200

    [emacs] move symbols to package

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -269,16 +269,6 @@
 (setq prettify-symbols-unprettify-at-point t)
 ;; Initialize mode
 (global-prettify-symbols-mode)
-;; Add replacements for `org-mode' task symbols
-(defun drot/org-prettify-task-symbols-setup ()
-  "Prettify `org-mode' task list symbols."
-  (dolist (symbol '(("TODO"     . ?⚑)
-                    ("DOING"    . ?⚐)
-                    ("CANCELED" . ?✘)
-                    ("DONE"     . ?✔)))
-    (cl-pushnew symbol prettify-symbols-alist :test #'equal)))
-;; Apply the custom hook
-(add-hook 'org-mode-hook #'drot/org-prettify-task-symbols-setup)
 
 ;;; Which function mode
 (setq which-func-unknown "n/a")
@@ -1058,6 +1048,16 @@ _d_: Subtree
           org-info
           org-id
           org-mhe))
+  ;; Add replacements for task symbols
+  (defun drot/org-prettify-task-symbols-setup ()
+    "Prettify `org-mode' task list symbols."
+    (dolist (symbol '(("TODO"     . ?⚑)
+                      ("DOING"    . ?⚐)
+                      ("CANCELED" . ?✘)
+                      ("DONE"     . ?✔)))
+      (cl-pushnew symbol prettify-symbols-alist :test #'equal)))
+  ;; Apply the custom hook
+  (add-hook 'org-mode-hook #'drot/org-prettify-task-symbols-setup)
   ;; Record time when a task is done
   (setq org-log-done 'time)
   ;; Indent headings by default
