eac772cc140eabf56c745774ec9ce995a74d1df9
Davor Rotim <rotim.davor@gmail.com>
2019-08-10 12:46:07 +0200

    [qutebrowser] update again

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -6,11 +6,6 @@
 # Uncomment this to still load settings configured via autoconfig.yml
 # config.load_autoconfig()
 
-# Aliases for commands. The keys of the given dictionary are the
-# aliases, while the values are the commands they map to.
-# Type: Dict
-c.aliases = {'w': 'session-save', 'q': 'quit', 'wq': 'quit --save'}
-
 # Enable JavaScript.
 # Type: Bool
 config.set('content.javascript.enabled', True, 'file://*')
