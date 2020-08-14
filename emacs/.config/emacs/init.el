d1af5dcfb490ccf8b0cb7facae5cd5b43bb527b8
Davor Rotim <rotim.davor@gmail.com>
2020-08-14 20:55:36 +0200

    [emacs] test new switch buffer function

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2067,14 +2067,57 @@
 (selectrum-mode +1)
 ;; Set key binding to repeat last command
 (global-set-key (kbd "C-x C-z") #'selectrum-repeat)
-;; Add `recentf' support
-(defun drot/selectrum-recentf-open ()
-  "Use `completing-read' to open a recent file."
+;; Switch to buffer or recent file
+(defun drot/selectrum-switch-buffer ()
   (interactive)
-  (let ((files (mapcar 'abbreviate-file-name recentf-list)))
-    (find-file (completing-read "Find recent file: " files nil t))))
+  (let* ((selectrum-should-sort-p nil)
+         (candidates
+          (let* ((cb (window-buffer
+                      (minibuffer-selected-window)))
+                 (bf (or (buffer-file-name cb) "")))
+            (lambda (input)
+              (let* ((buffers (mapcar #'buffer-name
+                                      (cl-delete-if
+                                       (lambda (buf)
+                                         (eq buf cb))
+                                       (buffer-list))))
+                     (files (cl-delete-if (lambda (f) (string= f bf))
+                                          (copy-sequence recentf-list)))
+                     (candidates ()))
+                (cond ((string-prefix-p " " input)
+                       (setq input (substring input 1))
+                       (setq candidates
+                             (cl-delete-if-not
+                              (lambda (name)
+                                (string-prefix-p " " name))
+                              buffers)))
+                      ((string-prefix-p "b " input)
+                       (setq input (substring input 2))
+                       (setq candidates
+                             (cl-delete-if
+                              (lambda (name)
+                                (string-prefix-p " " name))
+                              buffers)))
+                      ((string-prefix-p "f " input)
+                       (setq input (substring input 2))
+                       (setq candidates files))
+                      (t
+                       (setq candidates
+                             (append
+                              (cl-delete-if
+                               (lambda (name)
+                                 (string-prefix-p " " name))
+                               buffers)
+                              files))))
+                `((candidates . ,candidates)
+                  (input . ,input))))))
+         (cand (selectrum-read "Switch to: " candidates)))
+    (cond ((member cand recentf-list)
+           (find-file cand))
+          (t
+           (switch-to-buffer cand)))))
 ;; Set key binding
-(global-set-key (kbd "C-x C-r") #'drot/selectrum-recentf-open)
+(global-set-key [remap switch-to-buffer]  #'drot/selectrum-switch-buffer)
 ;; Configuration
 (after-load 'selectrum
   ;; Show line count
