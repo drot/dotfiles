92f5ea69ae9740fffd5f73c48f27276870ed20fe
Davor Rotim <rotim.davor@gmail.com>
2020-09-02 22:10:31 +0200

    [emacs] switch to fido

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -108,6 +108,16 @@
       read-buffer-completion-ignore-case t
       completion-ignore-case t)
 
+;;; Orderless completion style matching
+(straight-use-package 'orderless)
+;; Configuration
+(setq orderless-matching-styles
+      '(orderless-flex
+        orderless-strict-leading-initialism
+        orderless-regexp
+        orderless-prefixes
+        orderless-literal))
+
 ;;; Cycle completion on smaller number of candidates
 (setq completion-cycle-threshold 5)
 
@@ -122,6 +132,25 @@
 
 ;;; Indicate minibuffer recursion depth
 (minibuffer-depth-indicate-mode +1)
+
+;;; Icomplete
+(icomplete-mode +1)
+;; Configuration
+(after-load 'icomplete
+  ;; Reduce the completion delay
+  (setq icomplete-delay-completions-threshold 100
+        icomplete-compute-delay 0.2)
+  ;; Complete in other places as well
+  (setq icomplete-in-buffer t)
+  ;; Use single line display
+  (setq icomplete-prospects-height 1))
+
+;; Use `orderless' completion
+(add-hook 'icomplete-minibuffer-setup-hook
+          (lambda () (setq-local completion-styles '(orderless partial-completion))))
+
+;; FIDO mode for making Icomplete behave like IDO
+(fido-mode +1)
 
 ;;; Enable all disabled commands
 (setq disabled-command-function nil)
@@ -2019,74 +2048,10 @@
                   ("C-c p i" . hl-todo-insert-keyword)))
     (define-key hl-todo-mode-map (kbd (car bind)) (cdr bind))))
 
-;;; Selectrum minibuffer completion
-(straight-use-package 'selectrum)
-;; Initialize mode
-(selectrum-mode +1)
-;; Set key binding to repeat last command
-(global-set-key (kbd "C-x C-z") #'selectrum-repeat)
-;; Switch to buffer or recent file
-(defun drot/selectrum-switch-buffer ()
-  (interactive)
-  (let* ((selectrum-should-sort-p nil)
-         (candidates
-          (let* ((cb (window-buffer
-                      (minibuffer-selected-window)))
-                 (bf (or (buffer-file-name cb) "")))
-            (lambda (input)
-              (let* ((buffers (mapcar #'buffer-name
-                                      (cl-delete-if
-                                       (lambda (buf)
-                                         (eq buf cb))
-                                       (buffer-list))))
-                     (files (cl-delete-if (lambda (f) (string= f bf))
-                                          (copy-sequence recentf-list)))
-                     (candidates ()))
-                (cond ((string-prefix-p " " input)
-                       (setq input (substring input 1))
-                       (setq candidates
-                             (cl-delete-if-not
-                              (lambda (name)
-                                (string-prefix-p " " name))
-                              buffers)))
-                      ((string-prefix-p "b " input)
-                       (setq input (substring input 2))
-                       (setq candidates
-                             (cl-delete-if
-                              (lambda (name)
-                                (string-prefix-p " " name))
-                              buffers)))
-                      ((string-prefix-p "f " input)
-                       (setq input (substring input 2))
-                       (setq candidates files))
-                      (t
-                       (setq candidates
-                             (append
-                              (cl-delete-if
-                               (lambda (name)
-                                 (string-prefix-p " " name))
-                               buffers)
-                              files))))
-                `((candidates . ,candidates)
-                  (input . ,input))))))
-         (cand (selectrum-read "Switch to: " candidates)))
-    (cond ((member cand recentf-list)
-           (find-file cand))
-          (t
-           (switch-to-buffer cand)))))
-;; Set key binding
-(global-set-key [remap switch-to-buffer]  #'drot/selectrum-switch-buffer)
-;; Configuration
-(after-load 'selectrum
-  ;; Show line count
-  (setq selectrum-show-indices t)
-  ;; Show total and current matches
-  (setq selectrum-count-style 'current/matches))
-
 ;;; Amx
 (straight-use-package 'amx)
 ;; Change default backend to use `selectrum'
-(setq amx-backend 'selectrum)
+(setq amx-backend 'standard)
 ;; Initialize mode
 (amx-mode +1)
 ;; Set global key bindings
