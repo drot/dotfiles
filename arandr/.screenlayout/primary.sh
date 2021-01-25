a74f6bda6cad4d0693ccf8f2f36697731aff92d0
Davor Rotim <rotim.davor@nsoft.com>
2021-01-25 15:42:05 +0100

    update for new laptop

--- arandr/.screenlayout/primary.sh
+++ arandr/.screenlayout/primary.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 1920x1080 --pos 889x1200 --rotate normal --output HDMI-1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1-1 --mode 1920x1200 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 988x1200 --rotate normal --output DP-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-1 --off
